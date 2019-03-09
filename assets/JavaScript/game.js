3cd16bff25f3d72048e713723dbfa4ee5e1acbcd
John Garrett <infantrygarrett@icloud.com>
2019-03-08 19:26:10 -0600

    updated images, readme, and added JS file

---
+++ assets/JavaScript/game.js
@@ -0,0 +1,108 @@
+$(document).ready(function() {
+
+	var ranNum = [];           //random 
+    var crystals = [];       //crystals array
+    var randNumber;          // number to match
+	var crystalNumbers = []; // for array of random crystal values
+	var diamond;
+	var emerald;
+	var ruby;
+	var amethyst;
+    var totalScore = 0;      // user total score
+	var wins = 0;            //win score
+	var losses = 0;          //loss score
+
+	$("#endScreen").toggle();             //to toggle end screen hidden and display
+
+	for (var i = 19; i < 120; i++) {     //random number to match between 19 and 120
+		ranNum.push(i);
+	}  //end for random
+
+	for (var j = 1; j < 12; j++) {      //random number for crystals between 1 and 12
+		crystals.push(j);
+	}  //end for crystals
+	
+	function pickRandomNumber(arr) {   //function to get random number to match
+		var x = arr[Math.floor(Math.random() * arr.length)];
+		randNumber = x;
+		$("#randomNumber").html(randNumber);
+	} //end function random number
+
+	function pickRandomCrystals(arr) {  //function to get random number for crystals
+		for (var y = 0; y < 4; y++){
+			var a = arr[Math.floor(Math.random() * arr.length)];
+			crystalNumbers.push(a);
+		}
+    } //end function crystal number
+    
+	function crystalValues(arr) {	    //function crystal values	
+		for (i = 0; i < arr.length; i++) {
+		$("#button-" + (i+1)).attr("value", arr[i]);		
+		}  //end for 
+		diamond = arr[0];
+		emerald = arr[1];
+		ruby = arr[2];
+		amethyst = arr[3];
+	} //end function crystal
+	
+	pickRandomNumber(ranNum);            //call functions
+	pickRandomCrystals(crystals); 
+	crystalValues(crystalNumbers);
+	
+	$("#btnReplay").on("click", function() {	//replay end screen onclick	
+		$("#endScreen").hide();
+		reset();
+	});
+		
+	$("#diamond").on("click", function() {     //diamond onclick
+		totalScore += diamond;
+		$("#totalNumber").html(totalScore);
+	});  //end diamond
+
+	$("#emerald").on("click", function() {     //emerald onclick
+		totalScore += emerald;
+		$("#totalNumber").html(totalScore);
+	});  //end emerald
+
+	$("#ruby").on("click", function() {        //ruby onclick
+		totalScore += ruby;
+		$("#totalNumber").html(totalScore);
+	});  //end ruby
+
+	$("#amethyst").on("click", function() {    //amethyst onclick
+		totalScore += amethyst;
+		$("#totalNumber").html(totalScore);
+	});  //end amethyst
+
+	$(".gem").on("click", function() {         //wins
+		    	
+		if (totalScore == randNumber) {       //total score win
+			wins++;			
+			$("#totalNumber").html(totalScore);
+			$("#wins").html("Wins: " + wins);
+			$("#endScreen").toggle();
+			$("h2").html("CONGRATULATIONS!!!YOU WIN!");			
+			$("h3").html("Computer loses");			
+		}  //end win
+
+		else if (totalScore > randNumber){        //total score losses
+			losses++;
+			$("#totalNumber").html(totalScore);
+			$("#losses").html("Losses: " + losses);
+			$("#endScreen").toggle();
+			$("h2").text("SORRY,YOU LOST!!");
+			$("h3").html("Computer wins");			
+		}  //end loss
+	});  //end function game
+
+	function reset() {                           //function reset
+		crystalNumbers = [];           
+		pickRandomNumber(ranNum);
+		pickRandomCrystals(crystals);
+		crystalValues(crystalNumbers);
+		totalScore = 0;
+		$("#totalNumber").html(totalScore);				
+		$("#endContent").show();						
+	} //end function reset
+
+}); // end of script
